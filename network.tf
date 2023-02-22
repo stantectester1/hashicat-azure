4f13b951e36528a98676828a80f3a61ea91de1a9
Matt Jacobs <stantectester1@gmail.com>
2023-02-22 20:36:21 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/majacobs-training/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "gaurav-network-rg"
+  resource_group_name = "${var.prefix}-workshop"
 }
